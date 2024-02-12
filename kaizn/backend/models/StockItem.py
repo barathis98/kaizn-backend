c65f13f78da6327b3db0bc7302ab3591caf1a0f1
Barathi Sridhar <137120146+pulse-beep@users.noreply.github.com>
2024-02-12 11:11:12 -0500

    added itemstock

--- kaizn/backend/models/StockItem.py
+++ kaizn/backend/models/StockItem.py
@@ -1,8 +1,7 @@
-# myapp/models/stockitem.py
 from django.db import models
 
 class StockItem(models.Model):
-    item = models.ForeignKey('Item', on_delete=models.CASCADE)
+    item = models.ForeignKey('Item', on_delete=models.CASCADE, related_name='stock_items')
     in_stock = models.IntegerField()
     total_allocated = models.IntegerField()
     allocated_to_builds = models.IntegerField()
@@ -10,6 +9,8 @@ class StockItem(models.Model):
     available_stock = models.IntegerField()
     incoming_stock = models.IntegerField()
     net_stock = models.IntegerField()
+    min_stock = models.IntegerField(default=0, help_text="Minimum stock level before reordering.")
+    desired_stock = models.IntegerField(default=0, help_text="Desired stock level to maintain.")
 
     def __str__(self):
         return f"{self.item.name} - {self.in_stock} in stock"
